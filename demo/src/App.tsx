08c48c939b573c498bf155a49e6692360c4265ec
mostafa-aly25 <mostafaa@trustsecurenow.com>
2023-03-08 20:02:58 +0200

    Add cjs to the library

--- demo/src/App.tsx
+++ demo/src/App.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Box } from "@mostafa-aly25/react-package";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <Box color="red">Box</Box>
         <a
           className="App-link"
           href="https://reactjs.org"
